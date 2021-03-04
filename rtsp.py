b58def8c6b91a537677d6dd81adaec45b2e0e533
RenLuXi <38717072+RenLuXi@users.noreply.github.com>
2021-03-04 14:28:02 +0800

    Update rtsp.py
    api show

--- rtsp.py
+++ rtsp.py
@@ -1,14 +1,34 @@
-import pbcvt
 import cv2
-rtsp = pbcvt.Rtsp("rtsp address","user","passwd")
+import pbcvt
+'''
+pbcvt封装库API
+
+Rtsp(rtsp,user,passwd)
+初始化
+:param rtsp:rtsp流地址
+:param user:用户名
+:param passwd:密码
+:returns:
+
+rtsp_main()
+捕获码流并初始化配置
+:returns: ture or false
+
+ret_to_python()
+：returns: NULL or NDArray
+
+close()
+：returns: 0 代表成功关闭
+'''
+rtsp = pbcvt.Rtsp("rtsp://av_stream","admin","123456")
 print(" input parameters successfully ! ")
 temp = rtsp.rtsp_main()
-print(" loading finished ! ")
 while temp:
         img = rtsp.ret_to_python()
         if img is None:
                 print(" img null ! ")
-                res = rtsp.close()
+               # res = rtsp.close()
+                continue
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         cv2.imshow("test", img)
         cv2.waitKey(10)
